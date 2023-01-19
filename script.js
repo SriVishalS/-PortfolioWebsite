07e4c84d753fcda8480a10a24eb5eb6919d6b742
sriVishalS <vishalS221202@gmail.com>
2023-01-19 08:29:15 +0530

    changes Over Time

---
+++ script.js
@@ -0,0 +1,37 @@
+let icon = document.getElementById("icon");
+
+icon.addEventListener("click", function () {
+  icon.classList.toggle("fa-times");
+  icon.classList.toggle("fa-bars");
+});
+sendMail = () => {
+  let alertSuccess = document.getElementById("success");
+  let alertFail = document.getElementById("failed");
+  let usrObj = {
+    from_name: document.getElementById("name").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  usrObj.from_name.length > 0 &&
+  usrObj.email_id.length > 0 &&
+  usrObj.message.length > 0
+    ? emailjs.send("service_w5utfbw", "template_jixb2bl", usrObj).then(
+        function () {
+          debugger;
+          console.log("SUCCESS!");
+          alertSuccess.classList.add("show");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+          alertFail.classList.add("show");
+        }
+      )
+    : alert("You Havent Filled Your Login Creadentials Right");
+};
+var myVar;
+myFunction = () => {
+  myVar = setTimeout(loader, 2500);
+};
+loader = () => {
+  document.querySelector(".loadWrapper").style.display = "none";
+};
